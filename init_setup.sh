c840090cec6e100c1f67475a80968fce47acac4a
kristijanfistrek <fistrekkristijan@gmail.com>
2024-02-17 00:48:38 +0100

    Initial Commit

---
+++ init_setup.sh
@@ -0,0 +1,6 @@
+echo "[ `date` ]": "activate venv"
+.venv\Scripts\activate
+echo "[ `date` ]": "installing the requirements" 
+pip install -r requirements.txt
+echo "[ `date` ]": "Run the application" 
+flask --app app run --debug

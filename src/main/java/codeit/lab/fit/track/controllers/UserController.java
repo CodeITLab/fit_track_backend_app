f91cbb3154a2b56ccb1be13ee49c95773400820d
kristijanfistrek <fistrekkristijan@gmail.com>
2024-02-22 16:35:19 +0100

    Add: created controller for adding a user

--- src/main/java/codeit/lab/fit/track/controllers/UserController.java
+++ src/main/java/codeit/lab/fit/track/controllers/UserController.java
@@ -5,10 +5,7 @@ import codeit.lab.fit.track.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,13 +19,18 @@ public class UserController {
 
     @GetMapping("get-user-data")
     public ResponseEntity<List<User>> getUserData() {
-        try{
+        try {
             List<User> users = userService.findAll();
             return new ResponseEntity<>(users, HttpStatus.OK);
-
         } catch (Exception error) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("save-user-data")
+    public User saveUserData(@RequestBody User user) {
+        return userService.saveUser(user);
     }
 
 }

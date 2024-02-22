763db90302e5ce663b30a0927ddcf7a17bc39dc8
kristijanfistrek <fistrekkristijan@gmail.com>
2024-02-22 16:47:50 +0100

    Add: created get/set/toString methods for workout model

--- src/main/java/codeit/lab/fit/track/models/Workout.java
+++ src/main/java/codeit/lab/fit/track/models/Workout.java
@@ -13,4 +13,34 @@ public class Workout {
     @Column(name = "workout_name")
     private String workoutName;
 
+    public Workout() {}
+
+    public Workout(long id, String workoutName) {
+        this.id = id;
+        this.workoutName = workoutName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getWorkoutName() {
+        return workoutName;
+    }
+
+    public void setWorkoutName(String workoutName) {
+        this.workoutName = workoutName;
+    }
+
+    @Override
+    public String toString() {
+        return "Workout{" +
+                "id=" + id +
+                ", workoutName='" + workoutName + '\'' +
+                '}';
+    }
 }
